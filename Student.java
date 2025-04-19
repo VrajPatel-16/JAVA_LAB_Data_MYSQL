bc5792728649b8fccaee3b063379f9212caf29e5
Vraj Patel <vrajbpatel1719@gmail.com>
2025-04-20 01:04:56 +0530

    Method to display students detail

--- Student.java
+++ Student.java
@@ -18,14 +18,18 @@
          return name;  // Returns the student's name
      }
      
+ 
      // Setter method to update the student's PRN
      public void setPRN(long prn){
          this.prn = prn;  // Assigns the given PRN to the instance variable
-     }
- 
-     // Getter method to retrieve the student's PRN
-     public long getPRN() {
+
          return prn;  // Returns the student's PRN
      }
+ 
+     // Method to display the student's details (name and PRN)
+     public void display() {
+         System.out.println("Name: " + name + ", PRN: " + prn);  
+     }
+ 
  
  }
